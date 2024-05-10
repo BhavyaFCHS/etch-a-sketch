7b9c63668c19f30c2403b9dc946c4855fd88e9b0
Bhavya Chaudhary <chaudhary.bhavya2005@gmail.com>
2024-05-09 22:05:00 -0400

    Added function to create a grid of divs

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("loaded");
+const GRID_HEIGHT = 400;
+const GRID_WIDTH = 400;
+
+function createGrid(container, size) {
+    // container is a reference to a DOM node
+    for(let i = 0; i < size**2; i++) {
+        const square = document.createElement("div");
+        const width = GRID_WIDTH / size;
+        const height = GRID_HEIGHT / size;
+        square.style.width = `${width}px`;
+        square.style.width = `${height}px`;
+        container.append(square)
+    }
+}
+
+const container = document.querySelector(".container");
+container.style.width = `${GRID_WIDTH}px`;
+container.style.height = `${GRID_HEIGHT}px`;
+
+createGrid(container, 2);
