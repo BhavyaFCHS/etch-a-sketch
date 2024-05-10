9cf13e41647507fff8de6a954de8554db19c33f3
Bhavya Chaudhary <chaudhary.bhavya2005@gmail.com>
2024-05-09 23:05:46 -0400

    Added buttons to update grid and container size

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let gridSize = 400;
 
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
+const options = document.querySelector(".options");
+const gridSizeInput = document.querySelector("#grid-size");
+const containerSizeInput = document.querySelector("#container-size");
 
-container.style.width = `${gridSize}px`;
-container.style.height = `${gridSize}px`;
+function setGridSize(gridSize) {
+    container.style.width = `${gridSize}px`;
+    container.style.height = `${gridSize}px`;
+}
 
-function createGrid(container, size) {
+function createGrid(size) {
     // container is a reference to a DOM node
     for(let i = 0; i < size**2; i++) {
         const square = document.createElement("div");
@@ -24,10 +28,46 @@ function clearGrid() {
     );
 }
 
-createGrid(container, 60);
+function changeGridSize(size) {
+    container.innerHTML = "";
+    createGrid(size);
+}
+
+setGridSize(gridSize);
+createGrid(50);
 
 container.addEventListener("mouseover", e => {
     e.target.style.backgroundColor = "rgb(11, 44, 79)";
 });
 
-button.addEventListener("click", clearGrid);
+options.addEventListener("click", e => {
+    switch(e.target.id) {
+        case "clear-grid":
+            clearGrid();
+            break;
+        case "change-grid-size":
+            if(gridSizeInput.value) {
+                // make sure that the input value is between 1 and 100
+                (gridSizeInput.value > 100) ? changeGridSize(100) : 
+                (gridSizeInput.value < 1)   ? changeGridSize(1) : 
+                                      changeGridSize(gridSizeInput.value);
+            }
+            break;
+        case "change-container-size":
+            if(containerSizeInput.value) {
+                if(containerSizeInput.value > 1000) {
+                    gridSize = 1000;
+                    setGridSize(gridSize);
+                    changeGridSize(50);
+                } else if(containerSizeInput.value < 1) {
+                    gridSize = 1;
+                    setGridSize(gridSize);
+                    changeGridSize(1);
+                } else {
+                    gridSize = containerSizeInput.value;
+                    setGridSize(gridSize);
+                    changeGridSize(50);
+                }
+            }
+    }
+});
