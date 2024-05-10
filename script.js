127614c009fa08f06c34b3e0f4f7ba6031c5fc32
Bhavya Chaudhary <chaudhary.bhavya2005@gmail.com>
2024-05-09 22:25:17 -0400

    added button to clear the grid

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const GRID_HEIGHT = 400;
-const GRID_WIDTH = 400;
+let gridSize = 400;
+
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+container.style.width = `${gridSize}px`;
+container.style.height = `${gridSize}px`;
 
 function createGrid(container, size) {
     // container is a reference to a DOM node
     for(let i = 0; i < size**2; i++) {
         const square = document.createElement("div");
-        const width = GRID_WIDTH / size;
-        const height = GRID_HEIGHT / size;
+        const width = gridSize / size;
+        const height = gridSize / size;
         square.style.width = `${width}px`;
         square.style.width = `${height}px`;
         container.append(square)
     }
 }
 
-const container = document.querySelector(".container");
-container.style.width = `${GRID_WIDTH}px`;
-container.style.height = `${GRID_HEIGHT}px`;
+function clearGrid() {
+    container.childNodes.forEach(
+        n => n.style.backgroundColor = container.style.backgroundColor
+    );
+}
 
-createGrid(container, 2);
+createGrid(container, 60);
+
+container.addEventListener("mouseover", e => {
+    e.target.style.backgroundColor = "rgb(11, 44, 79)";
+});
+
+button.addEventListener("click", clearGrid);
